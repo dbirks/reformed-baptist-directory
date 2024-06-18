b9f9ed724f00c10a1b6287b9ad530be5f42f9048
David Birks <david@birks.dev>
2024-06-18 13:01:02 +0000

    Get initial search working

---
+++ mail-processor/main.py
@@ -0,0 +1,11 @@
+from langchain_community.tools.tavily_search import TavilySearchResults
+import dotenv
+
+dotenv.load_dotenv()
+
+search = TavilySearchResults(max_results=2)
+search_results = search.invoke("what is the weather in SF")
+print(search_results)
+# If we want, we can create other tools.
+# Once we have all the tools we want, we can put them in a list that we will reference later.
+tools = [search]
