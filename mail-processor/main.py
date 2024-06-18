2adb54046ee2688c75f5f3ff0b7a0da312498b99
David Birks <david@birks.dev>
2024-06-18 13:19:30 +0000

    More progress

--- mail-processor/main.py
+++ mail-processor/main.py
@@ -1,11 +1,62 @@
+# Walkthrough:   https://python.langchain.com/v0.2/docs/tutorials/agents/
+# Tavily search: https://app.tavily.com/home
+# LangSmith:     https://smith.langchain.com
+
+from pprint import pprint
+from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
 import dotenv
 
 dotenv.load_dotenv()
 
 search = TavilySearchResults(max_results=2)
-search_results = search.invoke("what is the weather in SF")
-print(search_results)
-# If we want, we can create other tools.
-# Once we have all the tools we want, we can put them in a list that we will reference later.
+# search_results = search.invoke("what is the weather in SF")
+# print(search_results)
+
 tools = [search]
+
+
+model = ChatOpenAI(model="gpt-4")
+
+
+# response = model.invoke([HumanMessage(content="hi")])
+# print(response.content)
+
+
+# model_with_tools = model.bind_tools(tools)
+
+
+# response = model_with_tools.invoke([HumanMessage(content="when's the 2024 election in the us")])
+
+# print(f"ContentString: {response.content}")
+# print(f"ToolCalls: {response.tool_calls}")
+
+
+agent_executor = create_react_agent(model, tools)
+
+
+# response = agent_executor.invoke({"messages": [HumanMessage(content="hi!")]})
+
+# print(response["messages"])
+
+
+# response = agent_executor.invoke(
+#     {"messages": [HumanMessage(content="whats the weather in sf?")]}
+# )
+# pprint(response["messages"])
+
+
+# for chunk in agent_executor.stream(
+#     {"messages": [HumanMessage(content="whats the weather in sf?")]}
+# ):
+#     print(chunk)
+#     print("----")
+
+
+
+
+
+
+# for next time, look into adding a tool to take in the email message body
